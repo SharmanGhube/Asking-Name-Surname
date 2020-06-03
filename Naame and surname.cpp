ece5ee8e55378f896d36f880bd2e2a2a55ea12b2
SharmanGhube <65918275+SharmanGhube@users.noreply.github.com>
2020-06-03 20:25:51 +0530

    Add files via upload

---
+++ Naame and surname.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "please enter your first and last name\n";
+	string first_name;
+	string last_name;
+	cin >> first_name >> last_name;
+	
+	cout << "My name is " << first_name << last_name << endl;
+	cout << "Hello world from " << first_name << endl;
+	cout << "Regards,"<< endl << last_name;
+		
+	system ("pause>0");
+}
+
